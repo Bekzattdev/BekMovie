d26d40df47cee10a9096b231a076c4815ee8651e
Bekzattdev <alpberlinfox@gmail.com>
2024-11-01 00:54:56 +0600

    header 90% finish

--- src/appPages/site/components/layout/header/Header.tsx
+++ src/appPages/site/components/layout/header/Header.tsx
@@ -4,47 +4,121 @@ import Link from "next/link";
 import logo from "../../../../../assets/logo/logo.ico";
 import Image from "next/image";
 import { links } from "@/constants/links";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { signOut, useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { useHeaderStore } from "@/stores/useHeaderStore";
+import { IoClose, IoMenu } from "react-icons/io5";
 
 const Header = () => {
   const pathname = usePathname();
+  const session = useSession();
+  const router = useRouter();
+  const [openMenu, setOpenMenu] = useState(false);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const { isMobile, setIsMobile } = useHeaderStore();
+
+  const changeIsMobile = () => {
+    setIsMobile(window.innerWidth <= 768);
+  };
+
+  useEffect(() => {
+    changeIsMobile();
+    window.addEventListener("resize", changeIsMobile);
+    return () => {
+      window.removeEventListener("resize", changeIsMobile);
+    };
+  }, []);
+
   return (
-    <header className={scss.Header}>
+    <header className={`${scss.Header} ${mobileMenu ? scss.active : ""}`}>
       <div className="container">
         <div className={scss.content}>
           <div className={scss.logo}>
-            <Link href={"/"}>
-              <Image src={logo} alt="Logo" />
+            <Link href="/">
+              <Image src={logo} alt="Logo" width={40} height={40} />
               <h2>BekMovie</h2>
             </Link>
           </div>
-          <nav>
-            <ul>
-              {links.map((item, index) => (
-                <p key={index}>
-                  <Link
-                    className={
-                      pathname === item.href
-                        ? `${scss.link} ${scss.active}`
-                        : `${scss.link}`
-                    }
-                    href={item.href}
+          <nav className={scss.nav}>
+            {!isMobile ? (
+              <ul>
+                {links.map((item, index) => (
+                  <p key={index}>
+                    <Link
+                      className={
+                        pathname === item.href
+                          ? `${scss.link} ${scss.active}`
+                          : scss.link
+                      }
+                      href={item.href}
+                    >
+                      {item.name}
+                    </Link>
+                  </p>
+                ))}
+                {session.data ? (
+                  <img
+                    onClick={() => setOpenMenu(!openMenu)}
+                    className={scss.userImage}
+                    src={session.data.user?.image!}
+                    alt="user"
+                  />
+                ) : (
+                  <button
+                    onClick={() => router.push("/api/auth/signin")}
+                    className={scss.signInButton}
                   >
-                    {item.name}
-                  </Link>
-                </p>
-              ))}
-              <div
-                className={
-                  pathname
-                    ? `${scss.signIn_col} ${scss.active}`
-                    : `${scss.signIn_col}`
+                    Sign in
+                  </button>
+                )}
+              </ul>
+            ) : (
+              <span
+                onClick={() => setMobileMenu(!mobileMenu)}
+                className={scss.mobileMenuIcon}
+              >
+                {mobileMenu ? <IoClose /> : <IoMenu />}
+              </span>
+            )}
+          </nav>
+          {openMenu && (
+            <div className={scss.openMenu}>
+              <button
+                onClick={() =>
+                  signOut({
+                    callbackUrl: "/",
+                  })
                 }
               >
-                <Link href={"/api/auth/signin"}>Sign In</Link>
-              </div>
-            </ul>
-          </nav>
+                Sign Out
+              </button>
+            </div>
+          )}
+          <div
+            onClick={() => setMobileMenu(false)}
+            className={`${scss.mobile} ${mobileMenu ? scss.active : ""}`}
+          >
+            <div className={scss.mobileLinks}>
+              {session.data ? (
+                <img
+                  onClick={() => setOpenMenu(!openMenu)}
+                  className={scss.userImage}
+                  src={session.data.user?.image!}
+                  alt="user"
+                />
+              ) : (
+                <button
+                  onClick={() => router.push("/api/auth/signin")}
+                  className={scss.signInButton}
+                >
+                  Sign in
+                </button>
+              )}
+              <Link href="/explore/movie">Movie</Link>
+              <Link href="/explore/tv">TV Shows</Link>
+            </div>
+          </div>
         </div>
       </div>
     </header>
